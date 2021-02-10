bcb38512ab1b80ea1b9b78d39b252d70b3436908
Anthony Gordon <anthony.gordon@bison.howard.edu>
2021-02-10 15:50:49 -0500

    Project started

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,TouchableWithoutFeedback, View,Image,SafeAreaView, Button } from 'react-native';
 
-export default function App() {
+export default function App() 
+{
+  const handlePress = () => console.log("Text was clicked");
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>
+        Hello React Native. 
+      </Text>
+      <TouchableWithoutFeedback onPress={() => console.log('Image was clicked')}>
+        <Image 
+           source={require("./assets/favicon.png")}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+      <Button title="Click Me" onPress={() => console.log("Button Tapped !")}/>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles = StyleSheet.create(
+  {
+  container: 
+  {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
