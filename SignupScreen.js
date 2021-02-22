a05d4e778cd1495e2d307093404e92d6506e68c1
Anthony Gordon <anthony.gordon@bison.howard.edu>
2021-02-22 11:51:25 -0500

    minor changes

--- SignupScreen.js
+++ SignupScreen.js
@@ -1,2 +1,5 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text,TouchableWithoutFeedback, 
     View,Image,SafeAreaView, Button, ScrollView } from 'react-native';
+
