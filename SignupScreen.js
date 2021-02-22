728b9022d097f18f6cbdf24fe636ac1dd51d414b
Anthony Gordon <anthony.gordon@bison.howard.edu>
2021-02-22 11:53:39 -0500

    cleared signup screen

--- SignupScreen.js
+++ SignupScreen.js
@@ -1,5 +1,1 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text,TouchableWithoutFeedback, 
-    View,Image,SafeAreaView, Button, ScrollView } from 'react-native';
 
