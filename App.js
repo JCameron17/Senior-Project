2bfbdd607e990e8bff6b927c157e4b48bb269376
Anthony Gordon <anthony.gordon@bison.howard.edu>
2021-02-23 14:27:50 -0500

    installed react stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet,Component, Text,TouchableWithoutFeedback, View,Image,SafeAreaView, Button, ScrollView } from 'react-native';
-import 'react-native-gesture-handler';
 
 export default function App() 
 {
