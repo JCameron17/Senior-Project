d999f9eb3b165ad9b68a9f339bc2febcaa8a24d1
Anthony Gordon <anthony.fgordon64@gmail.com>
2021-03-17 17:27:00 -0400

    added 'screens' folder & put HSceen and UPScreen

--- App.js
+++ App.js
@@ -6,38 +6,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
+import HomeScreen from './screens/HomeScreen';
+import UserProfile from './screens/UserProfile';
 
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button>
-        title = "Go to UserProfileScreen"
-        onPress= {() =>navigation.navigate("UserProflie")}
-      </Button>
-    </View>
-  );
-}
-
-function UserProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>UserProfleScreen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions=
+      {
+        {
+          headerStlye:
+          {
+            backgrounColor: 'green'
+          },
+          headerTintColor:'white',
+          headerTitleStyle:
+          {
+            fontWeight:'bold'
+          }
+        }
+      }>
+        <Stack.Screen name="Home" component={HomeScreen} options={}/>
         <Stack.Screen name="UserProfile" component={UserProfileScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+   </NavigationContainer>
   );
 }
 
---
+++ screens/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet,Component, Text,TouchableWithoutFeedback, View,Image,SafeAreaView, Button, ScrollView } from 'react-native';
+
+
+
+function UserProfileScreen() 
+{
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>UserProfleScreen</Text>
+      </View>
+    );
+  }
