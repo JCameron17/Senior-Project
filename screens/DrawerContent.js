848860d6f3902555f736852dd484907dd0a4308a
Anthony Gordon <anthony.fgordon64@gmail.com>
2021-03-18 00:39:02 -0400

    added DrawerContent.js file

---
+++ screens/DrawerContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet,Component, Text,TouchableWithoutFeedback, View,Image,SafeAreaView, Button, ScrollView } from 'react-native';
+import {DrawerContentScrollView,DrawerItem} from 'react-navigation/drawer';
+
+export function DrawerContent(props)
+{
+    return(
+        <View>
+            <Text> This is a test </Text>
+        </View>
+    );
+}
