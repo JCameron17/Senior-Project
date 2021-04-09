8e131dd98f2c22de7d485375ce9d8a7820417de2
Anthony Gordon <anthony.fgordon64@gmail.com>
2021-04-09 15:43:42 -0400

    added a config file

--- screens/Signin.js
+++ screens/Signin.js
@@ -1,3 +1,27 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyBTaEPxAXU85mYnifVklDJcHFPBjBiG9c0",
+//     authDomain: "young-money-b2c99.firebaseapp.com",
+//     projectId: "young-money-b2c99",
+//     storageBucket: "young-money-b2c99.appspot.com",
+//     messagingSenderId: "481209506884",
+//     appId: "1:481209506884:web:7013fd439a16d79cf37011",
+//     measurementId: "G-WSL0W83L80"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
+
 import React from 'react';
 import { 
     View,
