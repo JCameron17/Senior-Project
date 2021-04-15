e7f9394198a5ed83b2ce3ddc57e43c888c8efee8
Anthony Gordon <anthony.fgordon64@gmail.com>
2021-04-15 19:30:39 -0400

    added initLoginState and other login functions

--- App.js
+++ App.js
@@ -48,57 +48,64 @@ import { AuthContext } from './components/context';
 const Drawer = createDrawerNavigator();
 
 function App() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [userToken, setUserToken] = React.useState(null); 
+  // const [isLoading, setIsLoading] = React.useState(true);
+  // const [userToken, setUserToken] = React.useState(null); 
 
-  // const initialLoginState = {
-  //   isLoading: true,
-  //   userName: null,
-  //   userToken: null,
-  // };
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
 
-  // const loginReducer = (prevState, action) => {
-  //   switch( action.type ) {
-  //     case 'RETRIEVE_TOKEN': 
-  //       return {
-  //         ...prevState,
-  //         userToken: action.token,
-  //         isLoading: false,
-  //       };
-  //     case 'LOGIN': 
-  //       return {
-  //         ...prevState,
-  //         userName: action.id,
-  //         userToken: action.token,
-  //         isLoading: false,
-  //       };
-  //     case 'LOGOUT': 
-  //       return {
-  //         ...prevState,
-  //         userName: null,
-  //         userToken: null,
-  //         isLoading: false,
-  //       };
-  //     case 'REGISTER': 
-  //       return {
-  //         ...prevState,
-  //         userName: action.id,
-  //         userToken: action.token,
-  //         isLoading: false,
-  //       };
-  //   }
-  // };
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
 
-  // const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
 
   const authContext = React.useMemo(() => ({
-    signIn: () =>{
-      setUserToken('tony');
-      setIsLoading(false);
+    signIn: (userName,password) =>{
+      // setUserToken('tony');
+      // setIsLoading(false);
+      let userToken;
+      userName = null;
+      if (userName == 'user' && password == 'pass'){
+        userToken = 'lexi';
+      }
+      dispatch({type:'LOGIN,',id:userName, token:userToken});
     },
     signOut: () => {
-      setUserToken(null);
-      setIsLoading(false);
+      // setUserToken(null);
+      // setIsLoading(false);
+      dispatch({type:'LOGOUT'});
     },
     signUp: () => {
       setUserToken('tony');
@@ -108,31 +115,31 @@ function App() {
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false);
+     // setIsLoading(false);
+     dispatch({type:'RETRIEVE_TOKEN',token:'mario'});
     },1000);
   },[]);
 
-  if (isLoading){
+  if(loginState.isLoading ) {
     return(
-      <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size ="large"/>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size="large"/>
       </View>
     );
   }
 
   return (
     <AuthContext.Provider value = {authContext}>
-      <NavigationContainer>
-        {userToken != null ? (
-          <Drawer.Navigator drawerContent = {props => <DrawerContent {... props}/>}>
-            <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
-          </Drawer.Navigator> 
-
-        )
-        :
+    <NavigationContainer>
+      {loginState.userToken !== null ? (
+        <Drawer.Navigator drawerContent = {props => <DrawerContent {... props}/>}>
+          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        </Drawer.Navigator> 
+      )
+      :
         <RootStackScreen/>
-        }
-      </NavigationContainer> 
+      }
+    </NavigationContainer> 
     </AuthContext.Provider>
   );
 }
